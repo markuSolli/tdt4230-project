8f99ea7b318f1caa2105450ddbee0ea1ae0b70ca
markuSolli <markusped@hotmail.com>
2024-02-27 15:23:24 +0100

    Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
+#include <window.h>
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    // Initialise window
+    GLFWwindow* window = initialiseWindow();
 
-    return 0;
+    // Set background color
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Render loop
+    while(!glfwWindowShouldClose(window)) {
+        // Render
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers and poll for close event
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+    
+    return EXIT_SUCCESS;
 }
