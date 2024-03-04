b8a506b07b2d429a28ac967d3312c563f606c8d7
markuSolli <markusped@hotmail.com>
2024-03-04 19:52:27 +0100

    Use interval for hit tests

--- src/sphere.h
+++ src/sphere.h
@@ -2,13 +2,13 @@
 #define SPHERE_H
 
 #include "hittable.h"
-#include "vec3.h"
+#include "util.h"
 
 class sphere : public hittable {
   public:
     sphere(point3 _center, double _radius) : center(_center), radius(_radius) {}
 
-    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override {
+    bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
         vec3 oc = r.origin() - center;
         double a = r.direction().length_squared();
         double half_b = dot(oc, r.direction());
@@ -20,9 +20,9 @@ class sphere : public hittable {
 
         // Find the nearest root that lies in the acceptable range.
         double root = (-half_b - sqrtd) / a;
-        if (root <= ray_tmin || ray_tmax <= root) {
+        if (!ray_t.surrounds(root)) {
             root = (-half_b + sqrtd) / a;
-            if (root <= ray_tmin || ray_tmax <= root)
+            if (!ray_t.surrounds(root))
                 return false;
         }
 
