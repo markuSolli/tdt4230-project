a31c31a6da9c4ad495e2a17771f2a39805ccb822
markuSolli <markusped@hotmail.com>
2024-03-04 20:16:34 +0100

    Anti-aliasing

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@ int main() {
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 800;
+    cam.samples_per_pixel = 100;
+
     cam.render(world);
 
     return EXIT_SUCCESS;
