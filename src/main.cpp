01d9f656d4c1a8d91f0b9bcc7020ed367c3b7122
markuSolli <markusped@hotmail.com>
2024-04-03 08:22:07 +0200

    Render with 1024 samples

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,13 @@ int main() {
     world.add(make_shared<Sphere>(point3( -2.3, 0.0, 0), 1.0, material_left));
     world.add(make_shared<Mesh>("../res/models/suzanne.obj", material_center));
     world.add(make_shared<Sphere>(point3( 2.3, 0.0, 0.0), 1.0, material_right));
-    world.add(make_shared<Sphere>(point3( 5.0, 5.0, 5.0), 3.0, difflight));
+    world.add(make_shared<Sphere>(point3( 5.0, 5.0, 1.0), 3.0, difflight));
 
     Camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 720;
-    cam.samples_per_pixel = 512;
+    cam.samples_per_pixel = 1024;
     cam.max_depth         = 32;
     cam.fps               = 30;
 
@@ -35,7 +35,7 @@ int main() {
     cam.lookfrom = point3(0, 0.4, 3.5);
     cam.lookat   = point3(0, 0, 0);
     cam.vup      = vec3(0, 1, 0);
-    cam.background = color(0.2, 0.2, 0.2);
+    cam.background = color(0.1, 0.1, 0.1);
 
     auto start = steady_clock::now();
 
