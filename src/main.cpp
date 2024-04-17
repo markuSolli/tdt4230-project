576be620d39322c7db7e2720de1719179a7e7038
markuSolli <markusped@hotmail.com>
2024-04-17 19:47:10 +0200

    New render

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,10 @@ int main() {
     auto material_ground = make_shared<Lambertian>(color(0.8, 0.8, 0.0));
     auto material_table = make_shared<Lambertian>(color(0.351, 0.127, 0.047));
     auto material_plate = make_shared<Lambertian>(color(0.9, 0.9, 0.9));
-    auto material_glass = make_shared<Dielectric>(color(0.9, 1.0, 0.9), 1.3);
+    auto material_green_glass = make_shared<Dielectric>(color(0.92, 1.0, 0.92), 1.3);
+    auto material_blue_glass = make_shared<Dielectric>(color(0.92, 0.92, 1.0), 1.3);
     auto material_bowl = make_shared<Metal>(color(0.8, 0.8, 0.8), 0.5);
-    auto material_sun = make_shared<DiffuseLight>(color(1.0, 1.0, 1.0), 0.5);
-    auto material_light = make_shared<DiffuseLight>(color(1.0, 1.0, 1.0), 4.0);
+    auto material_light = make_shared<DiffuseLight>(color(1.0, 1.0, 1.0), 5.0);
 
     world.add(make_shared<Plane>(point3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0), material_ground));
     world.add(make_shared<Octree>("../res/models/table.obj", material_table, 3));
@@ -27,27 +27,26 @@ int main() {
     world.add(make_shared<Mesh>("../res/models/plate.obj", material_plate, point3(0.67, 1.0, -0.25)));
     world.add(make_shared<Mesh>("../res/models/plate.obj", material_plate, point3(-0.67, 1.0, 0.25)));
     world.add(make_shared<Mesh>("../res/models/plate.obj", material_plate, point3(-0.67, 1.0, -0.25)));
-    world.add(make_shared<Mesh>("../res/models/glass.obj", material_glass, point3(0.86, 1.0, 0.1)));
-    world.add(make_shared<Mesh>("../res/models/glass.obj", material_glass, point3(0.43, 1.0, -0.13)));
-    world.add(make_shared<Mesh>("../res/models/glass.obj", material_glass, point3(-0.43, 1.0, 0.1)));
-    world.add(make_shared<Mesh>("../res/models/glass.obj", material_glass, point3(-0.86, 1.0, -0.13)));
+    world.add(make_shared<Mesh>("../res/models/glass.obj", material_green_glass, point3(0.86, 1.0, 0.1)));
+    world.add(make_shared<Mesh>("../res/models/glass.obj", material_blue_glass, point3(0.43, 1.0, -0.13)));
+    world.add(make_shared<Mesh>("../res/models/glass.obj", material_blue_glass, point3(-0.43, 1.0, 0.1)));
+    world.add(make_shared<Mesh>("../res/models/glass.obj", material_green_glass, point3(-0.86, 1.0, -0.13)));
     world.add(make_shared<Mesh>("../res/models/bowl.obj", material_bowl, point3(0.0, 1.0, 0.0)));
-    world.add(make_shared<Plane>(point3(1000.0, 1000.0, 1000.0), vec3(-0.38, -0.66, -0.64), material_sun));
-    world.add(make_shared<Sphere>(point3(10.0, 10.0, 10.0), 6.0, material_light));
+    world.add(make_shared<Sphere>(point3(-10.0, 10.0, 10.0), 6.0, material_light));
 
     Camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 360;
-    cam.samples_per_pixel = 128;
+    cam.image_width       = 720;
+    cam.samples_per_pixel = 1024;
     cam.max_depth         = 32;
     cam.fps               = 30;
 
     cam.vfov     = 70;
-    cam.lookfrom = point3(0.6, 1.2, 0.8);
+    cam.lookfrom = point3(0.6, 1.3, 0.8);
     cam.lookat   = point3(-0.5, 0.4, -2.0);
     cam.vup      = vec3(0, 1, 0);
-    cam.background = color(0.05, 0.05, 0.05);
+    cam.background = color(0.1, 0.1, 0.1);
 
     int n_frames = 1;
     
